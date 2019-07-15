8e398a79bdaa2f45b0c98e9c73607cd2564913a7
HeDefine <hedingfei1993@126.com>
2019-07-15 17:56:50 +0800

    update 7th problem

--- demo7.py
+++ demo7.py
@@ -19,5 +19,24 @@
 # 输出: 21
 # 注意:
 #
-# 假设我们的环境只能存储得下 32 位的有符号整数，则其数值范围为 [−231,  231 − 1]。
+# 假设我们的环境只能存储得下 32 位的有符号整数，则其数值范围为 [−2^31,  2^31 − 1]。
 # 请根据这个假设，如果反转后整数溢出那么就返回 0。
+
+
+class Solution:
+    def reverse(self, x: int) -> int:
+        isOverZero = x >= 0
+        s = str(abs(x))
+        temp = list(s)
+        temp.reverse()
+        reverseStr = "".join(temp)
+        resultInt = int(reverseStr);
+        if not isOverZero:
+            resultInt = -1 * resultInt
+
+        if resultInt < -pow(2, 31) or resultInt > pow(2, 31) - 1:
+            resultInt = 0
+        return resultInt
+
+
+print(Solution().reverse(1534236469))
