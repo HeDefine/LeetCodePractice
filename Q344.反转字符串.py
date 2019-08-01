25447aaa932e5ffcf0eb098efdc856e4af7a1393
HeDefine <hedingfei1993@126.com>
2019-08-01 14:49:14 +0800

    update 344th problem

---
+++ Q344.反转字符串.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+
+# https://leetcode-cn.com/problems/reverse-string
+# 编写一个函数，其作用是将输入的字符串反转过来。输入字符串以字符数组 char[] 的形式给出。
+# 不要给另外的数组分配额外的空间，你必须原地修改输入数组、使用 O(1) 的额外空间解决这一问题。
+#
+# 你可以假设数组中的所有字符都是 ASCII 码表中的可打印字符。
+#
+# 示例 1：
+# 输入：["h","e","l","l","o"]
+# 输出：["o","l","l","e","h"]
+#
+# 示例 2：
+# 输入：["H","a","n","n","a","h"]
+# 输出：["h","a","n","n","a","H"]
+
+
+class Solution:
+    def reverseString(self, s: [str]):
+        idx0 = 0
+        idx1 = len(s) - 1
+        while idx0 < idx1:
+            s[idx0], s[idx1] = s[idx1], s[idx0]
+            idx0 += 1
+            idx1 -= 1
+
+
+print(Solution().reverseString(["h", "e", "l", "l", "o"]))  # ["o","l","l","e","h"]
+print(Solution().reverseString(["H", "a", "n", "n", "a", "h"]))  # ["h","a","n","n","a","H"]
