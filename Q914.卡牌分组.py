16a800da95b9cf01953d9447d20a99ba83df84db
Devine.He <Devine.He@refinedchina.com>
2019-08-30 11:23:22 +0800

    update 914th problem

--- Q914.卡牌分组.py
+++ Q914.卡牌分组.py
@@ -1,1 +1,64 @@
 #!/usr/bin/env python3
+
+# https://leetcode-cn.com/problems/x-of-a-kind-in-a-deck-of-cards
+# 给定一副牌，每张牌上都写着一个整数。
+# 此时，你需要选定一个数字 X，使我们可以将整副牌按下述规则分成 1 组或更多组：
+# 每组都有 X 张牌。
+# 组内所有的牌上都写着相同的整数。
+# 仅当你可选的 X >= 2 时返回 true。
+#
+# 示例 1：
+# 输入：[1,2,3,4,4,3,2,1]
+# 输出：true
+# 解释：可行的分组是 [1,1]，[2,2]，[3,3]，[4,4]
+#
+# 示例 2：
+# 输入：[1,1,1,2,2,2,3,3]
+# 输出：false
+# 解释：没有满足要求的分组。
+#
+# 示例 3：
+# 输入：[1]
+# 输出：false
+# 解释：没有满足要求的分组。
+#
+# 示例 4：
+# 输入：[1,1]
+# 输出：true
+# 解释：可行的分组是 [1,1]
+#
+# 示例 5：
+# 输入：[1,1,2,2,2,2]
+# 输出：true
+# 解释：可行的分组是 [1,1]，[2,2]，[2,2]
+#
+# 提示：
+# 1 <= deck.length <= 10000
+# 0 <= deck[i] < 10000
+
+from math import gcd
+
+
+class Solution:
+    def hasGroupsSizeX(self, deck: [int]) -> bool:
+        def canBeMod(allPurpose, target) -> bool:
+            for count in allPurpose:
+                if count % target != 0:
+                    return False
+            return True
+
+        temp = dict()
+        for n in deck:
+            temp[n] = temp.get(n, 0) + 1
+        allCount = list(temp.values())
+        for i in range(allCount[0], 1, -1):
+            if canBeMod(allCount, i):
+                return True
+        return False
+
+
+print(Solution().hasGroupsSizeX([1, 2, 3, 4, 4, 3, 2, 1]))  # True
+print(Solution().hasGroupsSizeX([1, 1, 1, 2, 2, 2, 3, 3]))  # False
+print(Solution().hasGroupsSizeX([1]))  # False
+print(Solution().hasGroupsSizeX([1, 1]))  # True
+print(Solution().hasGroupsSizeX([1, 1, 2, 2, 2, 2]))  # True
