fa46f08b3e20c0cfe534a76a8bee5d83e0e3836c
Devine.He <Devine.He@refinedchina.com>
2019-09-03 11:40:24 +0800

    update 994th problem

--- Q994.腐烂的橘子.py
+++ Q994.腐烂的橘子.py
@@ -1,1 +1,71 @@
 #!/usr/bin/env python3
+
+# https://leetcode-cn.com/problems/rotting-oranges
+# 在给定的网格中，每个单元格可以有以下三个值之一：
+# 值 0 代表空单元格；
+# 值 1 代表新鲜橘子；
+# 值 2 代表腐烂的橘子。
+# 每分钟，任何与腐烂的橘子（在 4 个正方向上）相邻的新鲜橘子都会腐烂。
+# 返回直到单元格中没有新鲜橘子为止所必须经过的最小分钟数。如果不可能，返回 -1。
+#
+# 示例 1：
+# 输入：[[2,1,1],[1,1,0],[0,1,1]]
+# 输出：4
+#
+# 示例 2：
+# 输入：[[2,1,1],[0,1,1],[1,0,1]]
+# 输出：-1
+# 解释：左下角的橘子（第 2 行， 第 0 列）永远不会腐烂，因为腐烂只会发生在 4 个正向上。
+#
+# 示例 3：
+# 输入：[[0,2]]
+# 输出：0
+# 解释：因为 0 分钟时已经没有新鲜橘子了，所以答案就是 0 。
+#
+# 提示：
+# 1 <= grid.length <= 10
+# 1 <= grid[0].length <= 10
+# grid[i][j] 仅为 0、1 或 2
+
+
+class Solution:
+    def orangesRotting(self, grid: [[int]]) -> int:
+        res = list()
+        temp = list()
+        orangeNum = 0
+        for x, line in enumerate(grid):
+            for y, each in enumerate(line):
+                if each == 2:
+                    temp.append((x, y))
+                elif each == 1:
+                    orangeNum += 1
+        res.append(temp)
+        while True:
+            temp = list()
+            for (x, y) in res[-1]:
+                if x + 1 < len(grid) and grid[x + 1][y] == 1:
+                    grid[x + 1][y] = 2
+                    temp.append((x + 1, y))
+                if y + 1 < len(grid[0]) and grid[x][y + 1] == 1:
+                    grid[x][y + 1] = 2
+                    temp.append((x, y + 1))
+                if x - 1 >= 0 and grid[x - 1][y] == 1:
+                    grid[x - 1][y] = 2
+                    temp.append((x - 1, y))
+                if y - 1 >= 0 and grid[x][y - 1] == 1:
+                    grid[x][y - 1] = 2
+                    temp.append((x, y - 1))
+            if len(temp) > 0:
+                orangeNum -= len(temp)
+                res.append(temp)
+            else:
+                break
+        if orangeNum > 0:
+            return -1
+        else:
+            return len(res) - 1
+
+
+print(Solution().orangesRotting([[2, 1, 1], [1, 1, 0], [0, 1, 1]]))  # 4
+print(Solution().orangesRotting([[2, 1, 1], [0, 1, 1], [1, 0, 1]]))  # -1
+print(Solution().orangesRotting([[0, 2]]))  # 0
